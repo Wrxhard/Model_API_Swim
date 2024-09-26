3b180f5fbbccde534dff7ff689a11401c78b82aa
Nguyen Phuc <phucmxst123@gmail.com>
2024-09-26 15:26:10 +0700

    init: fast api base

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+#Load env
+load_dotenv()
+
+app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",
+        host=os.getenv("HOST", "0.0.0.0"),
+        port=int(os.getenv("PORT", 8000)),
+        reload=os.getenv("DEBUG", "False").lower() == "true"
+    )
